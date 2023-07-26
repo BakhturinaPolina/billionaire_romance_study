fdeb004996cc0157c1cb9e529630a4ad3a892f9c
marfrlv <117598703+marfrlv@users.noreply.github.com>
2023-07-26 21:40:31 +0200

    liwc_novels.py

--- LIWC Novels Analysis/liwc_novels.py
+++ LIWC Novels Analysis/liwc_novels.py
@@ -1,134 +1,23 @@
-# the code allowing to analyze novels by chapters with LIWC
-
-# moint colab with google drive first
-from google.colab import drive
-drive.mount('/content/drive')
+'''The code below takes a list of preprocessed chapters (cleaned, lowered, tokenized, stopwords removed) for a book as input and
+returns a list of found LIWC categories with the number of words for each category. It analyzes each token in each list element
+and assigns it to one or more LIWC categories if it is found in the dictionary. 
+The code is adjusted to work in the Colab environment.'''
 
 # install the LIWC library
 ! pip install liwc
 
 # import other necessary libraries
 import liwc
-import nltk
-from nltk import word_tokenize
 # this is the path to a LIWC dictionary which is necessary for the library to work properly, the 2007 version can be easily found on the internet for free
-# dowmload it and put to the folder you are working with
+# dowmload it and put in the folder you are working with
 liwc_path = '/content/drive/MyDrive/billionaire_romance_study/code/LIWC2007_English100131.dic'
 parse, category_names = liwc.load_token_parser(liwc_path)
-import json
-import pandas as pd
+
+# to save the outputs as separate csv files
+import pandas as pd 
+
+# cleaning
 import re
-
-# get the paths to the files
-# insert a root directory
-# here it is a directory with toy novels without names of characters splitted by chapters
-rootdir = '/content/drive/MyDrive/billionaire_romance_study/toy_dataset_splitted_with_names'
-import os
-directory = rootdir
-file_paths = []
-
-for root, _, files in os.walk(directory):
-    for file in files:
-        file_path = os.path.join(root, file)
-        file_paths.append(file_path)
-# to check how many paths to files were found
-print(f'There are {len(file_paths)} paths found.')
-
-# sort the paths by alphanumerical order
-def alphanumeric_sort_key(element):
-    # split the element into alphabetical and numeric parts
-    parts = re.split(r'(\d+)', element)
-
-    # convert numeric parts to integers for proper numeric comparison
-    parts[1::2] = map(int, parts[1::2])
-
-    return parts
-
-# sort the paths
-file_paths_sorted = sorted(file_paths, key=alphanumeric_sort_key)
-# print the first five as an example
-for file_path in file_paths_sorted[:5]:
-  print(file_path)
-
-# group the chapters by the books they belong to
-book_chapters = {}
-
-for file_path in file_paths_sorted:
-    # extract the book name from the file path
-    book_name = os.path.basename(os.path.dirname(file_path))
-
-    # add the file path to the corresponding book's list of chapters
-    if book_name in book_chapters:
-        book_chapters[book_name].append(file_path)
-    else:
-        book_chapters[book_name] = [file_path]
-
-# iterate over all the books and their chapters
-for book, chapters in book_chapters.items():
-    print("Book:", book)
-    print("Chapters:", chapters)
-    print()
-
-# access the books' names and their chapters
-book_names = list(book_chapters.keys())
-len(book_names)
-book = book_names[...] # insert the index of an element here
-print(f'The book: {book_names[...]}.') # change here as well
-
-# access the number of chapters for a specific book
-num_chapters = len(book_chapters[book])
-print(f'The book contains {num_chapters} chapters.')
-
-# get the chapters' paths
-chapters_for_book = book_chapters[book]
-
-# create a list of chapters' texts
-def get_chapters_texts(chapters_file_paths):
-  list_of_texts = []
-  for chapter_file_path in chapters_file_paths:
-    text_file = open(chapter_file_path)
-    text = text_file.read()
-    list_of_texts.append(text)
-    text_file.close()
-  return list_of_texts
-
-book_list_of_texts = get_chapters_texts(chapters_for_book)
-print(f'There are {len(book_list_of_texts)} chapters in the list.')
-
-# preprocess the book chapters
-# tokenize with nltk
-import re
-import nltk
-nltk.download('punkt')  # Downloads the tokenizer data
-nltk.download('stopwords')  # Downloads stopwords data (optional)
-
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import string
-
-def preprocess_text(text):
-    # remove punctuation
-    text = text.translate(str.maketrans("", "", string.punctuation))
-
-    # convert text to lowercase
-    text = text.lower()
-
-    # tokenize the text
-    tokens = word_tokenize(text)
-
-    # remove stopwords
-    stop_words = set(stopwords.words('english'))
-    stop_words.update('’')
-    tokens = [token for token in tokens if token not in stop_words]
-
-    return tokens
-
-preprocessed_chapters = []
-for chapter in book_list_of_texts:
-  preprocessed_chapter = preprocess_text(chapter)
-  preprocessed_chapters.append(preprocessed_chapter)
-
-print(f'There are {len(preprocessed_chapters)} preprocessed chapters.')
 
 # check the categories in each chapter with LIWC
 from collections import Counter
@@ -139,6 +28,7 @@ for chapter in preprocessed_chapters:
   cat_dict = Counter.most_common(chapter_counts)
   recognized_categories.append(cat_dict)
   list_of_categories_by_book.append(recognized_categories)
+# print out the number of LIWC categories found for a given book
 print(len(list_of_categories_by_book))
 
 # save the list of categories by chapter as a csv file
@@ -146,59 +36,10 @@ categories = {'liwc categories': list_of_categories_by_book}
 df = pd.DataFrame(categories)
 df.index = range(1, len(df) + 1)
 df
-df.to_csv('shen_villain_liwc.csv', index=False)
+df.to_csv('***.csv', index=False) # insert the name of a file
 
-"""# LIWC categories in the whole toy dataset"""
-
-# create a list of chapters' texts
-def get_chapters_texts(chapters_file_paths):
-  list_of_texts = []
-  for chapter_file_path in chapters_file_paths:
-    text_file = open(chapter_file_path)
-    text = text_file.read()
-    list_of_texts.append(text)
-    text_file.close()
-  return list_of_texts
-
-texts = get_chapters_texts(file_paths_sorted)
-print(len(texts))
-
-# preprocess all the texts
-# preprocess the book chapters
-# tokenize with nltk
-import re
-import nltk
-nltk.download('punkt')  # Downloads the tokenizer data
-nltk.download('stopwords')  # Downloads stopwords data (optional)
-
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import string
-
-def preprocess_text(text):
-    # Remove punctuation
-    text = text.translate(str.maketrans("", "", string.punctuation))
-
-    # Convert text to lowercase
-    text = text.lower()
-
-    # Tokenize the text
-    tokens = word_tokenize(text)
-
-    # Remove stopwords
-    stop_words = set(stopwords.words('english'))
-    stop_words.update('’')
-    tokens = [token for token in tokens if token not in stop_words]
-
-    return tokens
-
-preprocessed_texts = []
-for text in texts:
-  preprocessed_text = preprocess_text(text)
-  preprocessed_texts.append(preprocessed_text)
-
-print(f'There are {len(preprocessed_texts)} preprocessed texts from the toy dataset.')
-
+'''The code below allows to extract LIWC categories from the whole dataset.
+The list of preprocessed chapters is first concatenated and then all the tokens are analyzed altogether.'''
 # concatenate the lists to work on all the tokens at once
 concatenated_list = []
 for sublist in preprocessed_texts:
@@ -216,10 +57,13 @@ for item in cat_dict_all:
 
 import pandas as pd
 
-# Specify the path to your CSV file
-csv_file = "/content/drive/MyDrive/billionaire_romance_study/code/liwc_categories.csv"
+# specify the path to your csv file
+# this is csv a file containing words and stems for the first 10 most frequent LIWC categories
+csv_file = ".../liwc_categories.csv"
 
-# Read the CSV file into a DataFrame
+# to work with the file, it has to be modified into a dictionary, where keys are LIWC category names and values are the words corresponding to them
+# the following code allows to implement all the necessary steps
+# read the csv file as a df
 df = pd.read_csv(csv_file, error_bad_lines=False, sep = ';')
 words_lists = []
 for i, words in enumerate(df):
@@ -236,7 +80,7 @@ print(len(words_lists))
 for sublist in words_lists:
   print(sublist)
 
-# delete asterstks with re
+# delete asterisks with re
 final_words_lists = []
 for sublist in words_lists:
   final_sublist = []
@@ -249,35 +93,36 @@ for sublist in words_lists:
   final_words_lists.append(final_sublist)
 
 # zip categories' names and the words corresponding to them and create dictionaries for the further analysis
-labels = ['relativ', 'cogmech', 'verb', 'affect', 'percept', 'bio', 'time', 'social', 'posemo', 'present']
+# 10 most frequent categories 
+labels = ['relativ', 'cogmech', 'verb', 'affect', 'percept', 'bio', 'time', 'social', 'posemo', 'present'] 
 words = final_words_lists
 cat_dictionaries = [{label: sublist} for label, sublist in zip(labels, words)]
 
-# Print the list of dictionaries
+# print the list of dictionaries
 for dictionary in cat_dictionaries:
     print(dictionary)
 
-# save the final dictioaries as a json file
+# save the final dictionaries as a json file
 import json
 with open("cat_dictionaries.json", "w") as file:
     json.dump(cat_dictionaries, file)
 
 import json
-file_path = '/content/drive/MyDrive/billionaire_romance_study/code/cat_dictionaries.json'
+file_path = '.../cat_dictionaries.json'
 with open(file_path) as file:
   cat_dictionaries = json.load(file)
 for dictionary in cat_dictionaries:
   print(dictionary)
 
-# define the function that return the corresponding words to a given category and a list of words
+# define the function that returns the corresponding words to a given category and a list of words
 # insert the index of a category from a list of dictionaries
 def get_matching_words(i, concatenated_list, file_name):
-  # access the list of terms for a given category
+  # access the list of terms for a given category [i]
   category = cat_dictionaries[i]
   category_terms = list(category.values())
   flattened_terms = category_terms[0]
 
-  # find the matches in a list of tokens and a list of category's terms
+  # find the matches in a list of tokens and a list of the category's terms
   category_matches = [word for word in concatenated_list if word in flattened_terms]
 
   # count them
@@ -299,5 +144,6 @@ def get_matching_words(i, concatenated_list, file_name):
         print(word, category_matches_counts[word], file=file)
 
 # 'relativ', 'cogmech', 'verb', 'affect', 'percept', 'bio', 'time', 'social', 'posemo', 'present'
-matches_sorted = get_matching_words(..., concatenated_list, '..._matches.txt') # insert an index of a categoty and its name to the title
+matches_sorted = get_matching_words(..., concatenated_list, '***_matches.txt') # insert an index of a categoty and its name to the title
+# all the matches found from the most to the least frequent terms
 matches_sorted
